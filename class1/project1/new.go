bd69e7b157f677422268a0e1822bedd07b65bda6
Diego Clair <diego93rodrigues@gmail.com>
2021-09-26 17:43:16 -0300

    feat: implemented algorith to verify if CPF (brazilian personal document indentification) is valid

---
+++ class1/project1/new.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var notNumberRE = regexp.MustCompile(`\D`)
+
+func IsValidDocumentNumber(document string) bool {
+
+	document = cleanNumber(document)
+
+	isCPF := len(document) == 11
+	isCNPJ := len(document) == 14
+	if !isCPF && !isCNPJ {
+		return false
+	}
+
+	if isCPF {
+		return validateCPF(document)
+	}
+	return validateCNPJ(document)
+}
+
+func validateCPF(cpf string) bool {
+
+	lenToFirstDigit := len(cpf) - 2
+	lenToSecondDigit := len(cpf) - 1
+
+	if invalidEqualNumbers(cpf) {
+		return false
+	}
+
+	firstDigit, _ := strconv.Atoi(cpf[lenToFirstDigit:lenToSecondDigit])
+	calculatedFirstDigit := calculateCPFDigit(cpf[:lenToFirstDigit], 10)
+	isCorrect := firstDigit == calculatedFirstDigit
+	if !isCorrect {
+		return false
+	}
+
+	secondDigit, _ := strconv.Atoi(cpf[lenToSecondDigit:])
+	calculatedSecondDigit := calculateCPFDigit(cpf[:lenToSecondDigit], 11)
+	isCorrect = secondDigit == calculatedSecondDigit
+	return isCorrect
+}
+
+func calculateCPFDigit(document string, multiplier int) int {
+	sum := 0
+	for i := 0; i < len(document); i++ {
+		pos, _ := strconv.Atoi(string(document[i]))
+		sum += pos * multiplier
+		multiplier--
+	}
+	rest := sum % 11
+	if rest < 2 {
+		return 0
+	}
+	return 11 - rest
+}
+
+func invalidEqualNumbers(document string) bool {
+	var allNumbersAreEqual bool
+	var digit string
+	for _, val := range document {
+		if len(digit) == 0 {
+			digit = string(val)
+		}
+		if string(val) == digit {
+			allNumbersAreEqual = true
+			continue
+		}
+		allNumbersAreEqual = false
+		break
+	}
+	return allNumbersAreEqual
+}
+
+func validateCNPJ(cnpj string) bool {
+
+	//need to be implemented
+	return true
+}
+
+// cleanNumber remove all not number characters
+func cleanNumber(value string) string {
+	return notNumberRE.ReplaceAllString(value, "")
+}
+
+func main() {
+	fmt.Println(IsValidDocumentNumber("111.111.111-11"))
+	fmt.Println(IsValidDocumentNumber("123.456.789-99"))
+	fmt.Println(IsValidDocumentNumber("935.411.347-80"))
+}
---
+++ class1/project1/new_test.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_invalidEqualNumbers(t *testing.T) {
+	type args struct {
+		document string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Equal numbers should return invalid=true",
+			args: args{
+				document: "11111111111",
+			},
+			want: true,
+		},
+		{
+			name: "Different numbers should return invalid=false",
+			args: args{
+				document: "11111311111",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := invalidEqualNumbers(tt.args.document); got != tt.want {
+				t.Errorf("invalidEqualNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_calculateCPFDigit(t *testing.T) {
+	type args struct {
+		document   string
+		multiplier int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "should return 8 for first digit",
+			args: args{
+				document:   "935411347",
+				multiplier: 10,
+			},
+			want: 8,
+		},
+		{
+			name: "should return 0 for second digit",
+			args: args{
+				document:   "9354113478",
+				multiplier: 11,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculateCPFDigit(tt.args.document, tt.args.multiplier); got != tt.want {
+				t.Errorf("calculateCPFDigit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_cleanNumber(t *testing.T) {
+	type args struct {
+		value string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Should return 0123456789",
+			args: args{
+				value: "er01.2-34y5.67-89",
+			},
+			want: "0123456789",
+		},
+		{
+			name: "Should return 11111111111",
+			args: args{
+				value: "111.111.111-11",
+			},
+			want: "11111111111",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cleanNumber(tt.args.value); got != tt.want {
+				t.Errorf("cleanNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_validateCPF(t *testing.T) {
+	type args struct {
+		cpf string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Valid Document 935.411.347-80",
+			args: args{
+				cpf: "93541134780",
+			},
+			want: true,
+		},
+		{
+			name: "Invalid Document 123.456.789-99",
+			args: args{
+				cpf: "12345678999",
+			},
+			want: false,
+		},
+		{
+			name: "Invalid Document 111.111.111-11",
+			args: args{
+				cpf: "11111111111",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateCPF(tt.args.cpf); got != tt.want {
+				t.Errorf("validateCPF() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidDocumentNumber(t *testing.T) {
+	type args struct {
+		document string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Valid Document 935.411.347-80",
+			args: args{
+				document: "935.411.347-80",
+			},
+			want: true,
+		},
+		{
+			name: "Invalid Document 123.456.789-99",
+			args: args{
+				document: "123.456.789-99",
+			},
+			want: false,
+		},
+		{
+			name: "Invalid Document 111.111.111-11",
+			args: args{
+				document: "111.111.111-11",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidDocumentNumber(tt.args.document); got != tt.want {
+				t.Errorf("IsValidDocumentNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
